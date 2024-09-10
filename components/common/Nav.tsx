be43365ef6837cbb58879b96f705d9d555124da3
olathedev <olapademicheal3@gmail.com>
2024-09-10 22:14:22 +0100

    implementing nav

--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -1,10 +1,23 @@
+import { Montserrat } from 'next/font/google'
 import React from 'react'
+import ElipseIcon from './icons/ElipseIcon'
 
 type Props = {}
 
+const monsterat = Montserrat({
+    subsets: ['latin']
+})
 const Nav = (props: Props) => {
   return (
-    <div>Nav</div>
+    <nav className='w-full bg-white h-[90px] flex items-center'>
+        <div className="container mx-auto flex justify-between">
+            <div className='relative flex pr-4'>
+                <h2 className={`${monsterat.className} text-[28px] font-bold text-primary `}>Blulearn
+                </h2>
+                <span className='absolute bottom-2.5 right-1'><ElipseIcon /></span>
+            </div>
+        </div>
+    </nav>
   )
 }
 
---
+++ components/common/icons/ElipseIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const ElipseIcon = (props: Props) => {
+    return (
+        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="4.89656" cy="5.27584" r="4.68966" fill="#F0C932" />
+        </svg>
+
+    )
+}
+
+export default ElipseIcon
