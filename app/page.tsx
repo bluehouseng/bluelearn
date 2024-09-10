075d561e3f5dc51e27c9425cd397db0cd51608ae
olathedev <olapademicheal3@gmail.com>
2024-09-10 21:33:17 +0100

    chore: setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,8 @@ import Maintenace from "@/public/svg/maintenance.svg";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col justify-center row-start-2 items-center sm:items-start">
-        <Image
-          className=""
-          src={Maintenace}
-          alt="Bluelearn logo"
-          width={280}
-          height={138}
-          priority
-        />
-        <div className="text-center w-full">
-          <h1 className="text-5xl font-bold">Bluelearn Africa</h1>
-          <p className="text-lg">...coming soon!!!</p>
-        </div>
-      </main>
-    </div>
+    <main>
+      
+    </main>
   );
 }
