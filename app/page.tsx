a223d0990d1e2b56c38f05162fa5a63e4ae971a1
Samagent97 <sammccarthy090@gmail.com>
2024-09-10 20:36:47 -0400

    Course-recommendation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Footer from "@/components/common/Footer";
 import Nav from "@/components/common/Nav";
+import CourseRecomendations from "@/components/ui/CourseRecomendations";
 
 export default function Home() {
   return (
     <main className="block w-full min-h-screen">
       <Nav />
+      <CourseRecomendations />
       <Footer/>
+      
+      
     </main>
   );
 }
