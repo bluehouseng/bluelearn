f8d9c9c369eeb476b63580e62d6b04366ee19cd8
mbragi <spontaneousmanji@gmail.com>
2024-09-11 01:16:45 +0100

    feat: implemented blog section

--- components/ui/BlogCard.tsx
+++ components/ui/BlogCard.tsx
@@ -16,10 +16,10 @@ function BlogCard({ date, description, imageUrl, title }: Readonly<Props>) {
   return (
     <div className="w-[402px]">
       <Image src={imageUrl} alt={title} width={402} height={285} />
-      <p className="text-lg font-medium mt-4">{date}</p>
+      <p className="text-lg font-medium mt-4 text-gray-500">{date}</p>
       <h3 className="text-xl font-medium text-wrap mt-4">{title}</h3>
-      <p className="text-sm mt-4">{truncate(description, 80)}</p>
-      <button className="relative mt-4">
+      <p className="text-sm mt-4 text-gray-500">{truncate(description, 80)}</p>
+      <button className="relative mt-6 font-medium text-[16px]">
         See More{" "}
         <span className="absolute top-0 left-20">
           <ArrowRight />
