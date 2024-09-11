7f9aafd76af02412910cf8b55f3169af3f1ee540
petergya <petergyang82@gmail.com>
2024-09-11 01:00:59 +0100

    chore: fix benefits

--- components/About.tsx
+++ components/About.tsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 function About() {
   return (
     <div className=" mx-10 pt-[13%]  flex justify-between ">
-      <div className="w-2/5 mt-16">
+      <div className="w-2/5 mt-20">
         <h2 className="text-xs">About us</h2>
-        <p className="font-semibold text-2xl mt-6">
+        <p className="text-3xl font-bold mt-6">
             Bluelearn Free E-Learning Service To Help You Grow
         </p>
         
         </div>
         <div className="">
-        <Image src="/video.png" alt="" width={500} height={500} />
+        <Image src="/video.png" alt="" width={550} height={500} />
         </div>
     </div>
   );
