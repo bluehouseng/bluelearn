ba3749347e3682a295341ff58669543c588965a9
Samagent97 <sammccarthy090@gmail.com>
2024-09-12 08:20:39 -0400

    dont bother

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Footer from "@/components/common/Footer";
 import Nav from "@/components/common/Nav";
 import CourseRecomendations from "@/components/ui/CourseRecomendations";
+import Testimonials from "@/components/ui/Testimonials";
 
 export default function Home() {
   return (
     <main className="block w-full min-h-screen">
-      <Nav />
+      {/* <Nav />
       <CourseRecomendations />
-      <Footer/>
+      <Footer/> */}
+      <Testimonials/>
       
       
     </main>
---
+++ components/ui/Testimonials.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Testimonials() {
+  return (
+    <div className='flex justify-between bg-[#EEF4FA] m-16'>
+     <div>
+        <p className='text-[#282938] font-bold pb-12 pt-16' >Testimonials</p>
+        <p className='text-[#282938] pb-8' >Blulearn has been trusted by more than 10,000 students</p>
+     </div>
+     <div>
+        <p className='w-[490px] pb-12 pt-16 text-[#282938]' > "The material presented is easy to understand, the quality of the instructors is excellent, and their response is quick. So, I highly recommend the courses here!!" </p>
+        <div>
+<p>Andrew Festus</p>
+        </div>
+        </div> 
+    </div>
+  )
+}
+
+export default Testimonials
