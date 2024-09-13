f88fd2d1e84272d9665d1ae367dbd7249c416c10
Samagent97 <sammccarthy090@gmail.com>
2024-09-13 17:53:40 -0400

    checking fix

--- components/ui/ContactForm.tsx
+++ components/ui/ContactForm.tsx
@@ -19,35 +19,39 @@ function ContactForm() {
             </p>
           </div>
         </div>
-        <div className="relative max-w-lg flex-1 bg-[#2489D3] p-6 text-white">
-          <p className="text-xl font-semibold mb-2">Register Yourself!</p>
-          <p className="text-md mb-4">Prepare yourself for a future full of stars</p>
-          <form action="" className="flex flex-col space-y-4">
+        <div className="relative max-w-lg flex-1 bg-[#2489D3]  text-white flex items-center justify-center">
+         
+
+         <div className="" >    
+          <p className="text-xl font-semibold mt-2 mb-4 ">Register Yourself!</p>
+          <p className="text-sm mb-4">Prepare yourself for a future full of stars</p>
+          <form action="" className="flex flex-col space-y-4 ">
             <input
               type="text"
               name="Your Name"
               placeholder="Your Name"
-              className="p-4 bg-transparent text-white placeholder-opacity-80 border border-white rounded w-full"
+              className="p-2 bg-transparent text-white placeholder-opacity-80 border border-white rounded w-[70%]"
             />
             <input
               type="email"
               name="Email"
               placeholder="Email"
-              className="p-4 bg-transparent text-white placeholder-opacity-80 border border-white rounded w-full"
+              className="p-4 bg-transparent text-white placeholder-opacity-80 border border-white rounded w-[70%]"
             />
             <input
               type="password"
               name="Password"
               placeholder="Password"
-              className="p-4 bg-transparent text-white placeholder-opacity-80 border border-white rounded w-full"
+              className="p-4 bg-transparent text-white placeholder-opacity-80 border border-white rounded w-[70%]"
             />
             <button
               type="submit"
-              className="p-4 bg-[#F0C932] text-white rounded hover:bg-[#d4b800]"
+              className="p-4 bg-[#F0C932] text-white rounded hover:bg-[#d4b800] w-[70%]"
             >
               Register
             </button>
           </form>
+          </div>
         </div>
       </div>
     </div>
