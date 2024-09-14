8adfa75c19ec5071b678aa8c46070537eec25bf0
mbragi <spontaneousmanji@gmail.com>
2024-09-14 22:51:00 +0100

    Refactor tailwind.config.ts and add custom-light boxShadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,9 @@ const config: Config = {
         unijos: "url('/images/unijos.png')",
         koenig: "url('/images/koenig.png')",
       },
+      boxShadow: {
+        'custom-light': '0px 8px 24px rgba(149, 157, 165, 0.2)',
+      },
     },
   },
   plugins: [],
