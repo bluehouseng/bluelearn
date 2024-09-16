29761c5ab494534e86c89bc95cad5fa896e05f43
Samagent97 <sammccarthy090@gmail.com>
2024-09-16 09:33:50 -0400

    editing

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
 import About from "@/components/About";
-import Benefit from "@/components/Benefit";
 import Blog from "@/components/Blog";
 import Footer from "@/components/common/Footer";
 import Nav from "@/components/common/Nav";
-import CourseRecomendations from "@/components/ui/CourseRecomendations";
 import Hero from "@/components/Hero";
 import Course from "@/components/Course";
-// import Mentors from "@/components/Instructors";
 import Partners from "@/components/Partners";
 import Instructors from "@/components/Instructors";
-
 import ContactForm from "@/components/ui/ContactForm";
-
 import Faq from "@/components/Faq";
+import AnimatedComponent from "@/components/common/AnimatedScrollComponent";
 
 
 export default function Home() {
@@ -20,16 +16,23 @@ export default function Home() {
     <main className="w-full min-h-screen">
       <Nav />
       <Hero />
+      <AnimatedComponent id="partners">
       <Partners />
-      <About />
-      
-      {/* <CourseRecomendations /> */}
-      <Faq />
-      <Benefit />
-      <Instructors />
-      <Course />
-      <Blog/>
-      <ContactForm />
+      </AnimatedComponent>
+      <AnimatedComponent id="about">
+        <About />
+      </AnimatedComponent>
+        <Course />
+      <AnimatedComponent id="faq">
+        <Faq />
+      </AnimatedComponent>
+      <AnimatedComponent id="instructors">
+        <Instructors />
+      </AnimatedComponent>
+      <AnimatedComponent id="contact-form">
+        <ContactForm />
+      </AnimatedComponent>
+        <Blog />
       <Footer/> 
       
     </main>
